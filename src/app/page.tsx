a82a2085dae85ad9b4fb6197e7456fade7e3b123
kunaljain-debug <kunaljain@grenerobotics.com>
2025-06-28 18:32:54 +0530

    feat: add Prisma integration with database schema and initial migrations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-const page = () => {
+const page = async () => {
+
+  const posts = await prisma.user.findMany();
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4 text-red-500">Welcome to My App</h1>
-      <p className="text-lg text-gray-700">This is a simple Next.js application.</p>
-      <Button variant={"destructive"} size={"lg"} className="mt-6 px-8">
-        Click Me
-      </Button>
+    <div>
+      `${JSON.stringify(posts, null, 2)}`
     </div>
   );
 }
